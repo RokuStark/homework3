e2c1f62196baa998c6721062d42db51b34d28e5c
rokustark <rokustark@gmail.com>
2021-06-01 12:01:56 +0300

    Generate CMake and make files

---
+++ CMakeFiles/lessen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lessen.dir/main.cpp.o"
+  "lessen"
+  "lessen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lessen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
